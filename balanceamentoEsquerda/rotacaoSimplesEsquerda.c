1a5a5d811e78f2cca1692004f9e3194f0d54dec7
Leandro Bergamini dos Santos Júnior <144055259+LeandroBergamini@users.noreply.github.com>
2023-09-19 13:08:36 -0300

    Update rotacaoSimplesEsquerda.c

--- balanceamentoEsquerda/rotacaoSimplesEsquerda.c
+++ balanceamentoEsquerda/rotacaoSimplesEsquerda.c
@@ -1,7 +1,7 @@
 Nodo *rotacaoSimplesEsquerda(Nodo *nodo)
 {
     Nodo *auxiliar = nodo->filhoDireita;
-    nodo->filhoDireita = backup->filhoEsquerda;
-    backup->filhoEsquerda = nodo;
+    nodo->filhoDireita = auxiliar->filhoEsquerda;
+    auxiliar->filhoEsquerda = nodo;
     return auxiliar;
 }
